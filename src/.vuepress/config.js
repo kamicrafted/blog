5d63be165c5b7e25c5769d9fb58c0e29ff84659b
Davdi Yoon <hello@kamicrafted.com>
2019-10-04 18:54:47 -0400

    started conversion of stylus to scss and theming the blog

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,50 +1,56 @@
-const currentDateUTC = new Date().toUTCString()
+const currentDateUTC = new Date().toUTCString();
 
 module.exports = {
-	title: 'Pixels & Bits – Kamicrafted',
-	dest: './public',
-	themeConfig: {
-		// repo: 'https://wwww.github.com',
-		// repoLabel: 'Repo',
-		editLinks: false,
-		editLinkText: 'Found a bug? Help me improve this page!',
-		nav: [
-			// { text: 'Home', link: '/' }, 
-			{ text: 'Posts', link: '/blog/' },
-			{ text: 'Archive', link: '/archive/' },
-			{ text: 'RSS', link: '/rss.xml' }
-		],
-		logo: '/vuepress-blog-logo.png',
-		docsDir: 'src',
-		pageSize: 5,
-		startPage: 0
-	},
-	plugins: [
-		[
-			'@vuepress/google-analytics',
-			{
-				ga: '' // UA-00000000-0
-			}
-		],
-		[
-			'vuepress-plugin-rss',
-			{
-				base_url: '/',
-				site_url: 'https://vuepressblog.org',
-				filter: frontmatter => frontmatter.date <= new Date(currentDateUTC),
-				count: 20
-			}
-		],
-		'vuepress-plugin-reading-time',
-		'vuepress-plugin-janitor'
-	],
-	head: [
-		['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon.png' }],
-		['link', { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' }],
-		['link', { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' }],
-		['link', { rel: 'manifest', href: '/site.webmanifest' }],
-		['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }],
-		['meta', { name: 'msapplication-TileColor', content: '#da532c' }],
-		['meta', { name: 'theme-color', content: '#ffffff' }]
-	]
-}
+  title: "Pixels & Bits – Kamicrafted Blog",
+  dest: "./public",
+  themeConfig: {
+    // repo: 'https://wwww.github.com',
+    // repoLabel: 'Repo',
+    editLinks: false,
+    editLinkText: "Found a bug? Help me improve this page!",
+    nav: [
+      // { text: 'Home', link: '/' },
+      { text: "Posts", link: "/blog/" },
+      { text: "Archive", link: "/archive/" },
+      { text: "RSS", link: "/rss.xml" }
+    ],
+    logo: "/logo-pixelsnbits.svg",
+    docsDir: "src",
+    pageSize: 5,
+    startPage: 0
+  },
+  plugins: [
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-179348-4" // UA-00000000-0
+      }
+    ],
+    [
+      "vuepress-plugin-rss",
+      {
+        base_url: "/",
+        site_url: "https://blog.kamicrafted.com",
+        filter: frontmatter => frontmatter.date <= new Date(currentDateUTC),
+        count: 20
+      }
+    ],
+    "vuepress-plugin-reading-time",
+    "vuepress-plugin-janitor"
+  ],
+  head: [
+    [
+      "link",
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-icon.png" }
+    ],
+    ["link", { rel: "icon", sizes: "32x32", href: "/favicon-32x32.png" }],
+    ["link", { rel: "icon", sizes: "16x16", href: "/favicon-16x16.png" }],
+    ["link", { rel: "manifest", href: "/site.webmanifest" }],
+    [
+      "link",
+      { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" }
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#da532c" }],
+    ["meta", { name: "theme-color", content: "#ffffff" }]
+  ]
+};
--- src/blog/my-first-post.md
+++ src/blog/my-first-post.md
@@ -1,15 +1,38 @@
 ---
 title: My First Post
 date: 2018-12-23 20:47:18
-excerpt: Here is a little bit about my post.
+excerpt: Dr. Zoidberg, that doesn't make sense. But, okay! No! The kind with looting and maybe starting a few fires! Well, then good news! It's a suppository. Hey, guess what you're accessories to. Hey, guess what you're accessories to.
 type: post
 blog: true
 tags:
-    - HTML
-    - CSS
-    - JavaScript
+  - HTML
+  - CSS
+  - JavaScript
 ---
 
 ## Hello Dr. Zoidberg
 
 Dr. Zoidberg, that doesn't make sense. But, okay! No! The kind with looting and maybe starting a few fires! Well, then good news! It's a suppository. Hey, guess what you're accessories to. Hey, guess what you're accessories to.
+
+`This is a piece of code` as part of a sentence.
+
+```css
+<style>
+@media (min-width: $MQMobile) {
+  .nav-links a {
+    &:hover,
+    &.router-link-active {
+      color: $textColor;
+    }
+  }
+
+  .nav-item > a:not(.external) {
+    &:hover,
+    &.router-link-active {
+      margin-bottom: -2px;
+      border-bottom: 2px solid $accentColor;
+    }
+  }
+}
+</style>
+```
